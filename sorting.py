324fc2a4c5f1c5b3121fb2d9903ba180292c4b09
GavinR <riggigav000@alpinesd.org>
2024-10-15 13:12:08 -0600

    Update sorting.py
    Updated bubble sort.

--- sorting.py
+++ sorting.py
@@ -1,16 +1,20 @@
 #bubble(list) - > sorted ist
 
 #selection(list) -> sorted list
-def bubble(unsorted_list):
-    for i in range(0, len(unsorted_list)):
-        for e in range(i, len(unsorted_list)):
-            if unsorted_list[e] > unsorted_list[e]:
-                temp_var = unsorted_list[e]
+def bubble(input_list):
+    """Sort via bubble sort"""
+    # Note: Two nested loops that loop the same thing feels wrong. 
+    for e in range(0, len(input_list)):
 
-                unsorted_list[e] = unsorted_list[i]
-                unsorted_list[i] = temp_var
+        for i in range(0, len(input_list) - e): # e-1 ignores the previous searches
+            if e != 0 and input_list[i + 1] < input_list[i]:
+                # Store i+1 so we could switch it with i without any problems
+                storage_var = input_list[i + 1]
+                input_list[i+1] = input_list[i]
+                input_list[i] = storage_var
 
-    return unsorted_list
+    # Return the sorted list for future use later
+    return input_list    
 
 
 def selection(unsorted_list):
